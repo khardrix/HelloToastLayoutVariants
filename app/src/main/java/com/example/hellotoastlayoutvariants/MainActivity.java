baef5684059a3197f98adbcb9f02764a3af9d98c
khardrix <carmenandryanhuffman@yahoo.com>
2021-09-20 01:14:16 -0400

    first commit

--- app/src/main/java/com/example/hellotoastlayoutvariants/MainActivity.java
+++ app/src/main/java/com/example/hellotoastlayoutvariants/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.hellotoast;
+package com.example.hellotoastlayoutvariants;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
--- app/src/test/java/com/example/hellotoastlayoutvariants/ExampleUnitTest.java
+++ app/src/test/java/com/example/hellotoastlayoutvariants/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.hellotoast;
+package com.example.hellotoastlayoutvariants;
 
 import org.junit.Test;
 
